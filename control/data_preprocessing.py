85fdb5f949bbe287697dd94142ebef36a514a9ab
luzhangao <luzhangao@gmail.com>
2021-12-21 21:29:09 -0500

    Update data preprocessing

--- control/data_preprocessing.py
+++ control/data_preprocessing.py
@@ -9,7 +9,7 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from utils.gerenal_tools import open_yaml, save_pickle
 
 
@@ -18,11 +18,30 @@ paras = open_yaml("../data/samples.yaml")
 
 def preprocessing():
     raw_data = paras["raw_path"] + paras["train_path"] + paras["train_file"]
-    df = pd.read_csv(raw_data, sep=";")
+    # df = pd.read_csv(raw_data, sep=";")
+    df = pd.read_excel(raw_data)
+    # Remove all data from respondents: parents (Dr.K)
+    df = df[df["Type of person"] != "parents"]
+    # Fill nan with mean
+    df = df.fillna(df.mean(numeric_only=True))
     print(df)
+    # Split the dataframe
+    cdf = df[paras["continuous_columns"]]
+    ddf = df[paras["discrete_columns"]]
+
+    scaler = MinMaxScaler()
+    carray = scaler.fit_transform(cdf)
+    c_feature_names = scaler.get_feature_names_out()
+
     enc = OneHotEncoder(handle_unknown='ignore')
-    res = enc.fit_transform(df).toarray()
-    feature_names = enc.get_feature_names_out()
+    darray = enc.fit_transform(ddf).toarray()
+    d_feature_names = enc.get_feature_names_out()
+
+    # print(carray.shape, darray.shape)
+    res = np.concatenate([carray, darray], axis=1)
+    feature_names = np.concatenate([c_feature_names, d_feature_names])
+    # print(res.shape)
+    # print(feature_names)
     np.save(paras["raw_path"] + paras["train_path"] + paras["train_dataset"], res)
     save_pickle(feature_names, paras["model_saved_path"] + paras["feature_names"])
     return res
